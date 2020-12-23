44834321b55ad3673809586482f83db4d2b21d6c
Kinar Sharma <56648862+EGOR-IND@users.noreply.github.com>
2020-12-23 23:07:29 +0530

    Initial Commit

---
+++ app/src/main/java/egor_ind/kotlin/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package egor_ind.kotlin.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

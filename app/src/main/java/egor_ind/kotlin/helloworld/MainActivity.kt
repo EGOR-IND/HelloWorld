c4944dc08925ea6ec4a2576676de6646897c122d
EGOR-IND <56648862+EGOR-IND@users.noreply.github.com>
2021-02-16 20:55:18 +0530

    add snackbar for toasting

--- app/src/main/java/egor_ind/kotlin/helloworld/MainActivity.kt
+++ app/src/main/java/egor_ind/kotlin/helloworld/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     lateinit var editText: EditText
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
             if (editText.text.isNotEmpty()){
                 textView.setText("Hello ${editText.text}")
             } else {
-                Toast.makeText(this@MainActivity, "Enter Name in the field given", Toast.LENGTH_LONG).show()
+                Snackbar.make(findViewById(android.R.id.content), "Enter Name in the field given", Snackbar.LENGTH_LONG).show()
+//                Toast.makeText(this@MainActivity, "Enter Name in the field given", Toast.LENGTH_LONG).show()
             }
         }
     }
